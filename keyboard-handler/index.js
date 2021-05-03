04c45026e24668082e6ff943d07be65d50f18ed2
Mariano Mangialavori <mariann_03@outlook.com>
2021-05-03 14:52:21 -0300

    Update index.js

--- keyboard-handler/index.js
+++ keyboard-handler/index.js
@@ -3,9 +3,7 @@ const sdk = require('cue-sdk')
 
 sdk.CorsairPerformProtocolHandshake()
 const errCode = sdk.CorsairGetLastError()
-if (errCode === 0) {
-	console.log('succsex!')
-}
+if (!errCode) console.log('succses!')
 
 io.on('connection', (socket) => {
 	socket.on('keyboard', (data) => {
